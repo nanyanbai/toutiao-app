0a525ca7fe1a47248c4e4e7fdb96df4fb553a53d
nanweiwei <1115001559@qq.com>
2020-09-27 10:20:44 +0800

    'login页面'

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: '' // 基础路径
+  baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
 })
 
 export default request
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,58 @@
 <template>
     <div class="login-container">
-      登录页面
+      <van-nav-bar
+        title="登录"
+        lett-arrow
+        @click-left="$router.back()"
+      />
+
+      <!--登录表单-->
+      <van-form @submit="onLogin">
+        <van-cell-group>
+          <van-field
+            v-model="user.mobile"
+            clearable
+            center
+            left-icon="smile-o"
+            placeholder="请输入手机号"
+          />
+          <van-field
+            v-model="user.code"
+            clearable
+            center
+            left-icon="music-o"
+            placeholder="请输入验证码"
+          >
+            <template #button>
+              <van-button size="small" type="primary">发送验证码</van-button>
+            </template>
+          </van-field>
+        </van-cell-group>
+        <van-button round block size="small" type="info" native-type="submit">
+          提交
+        </van-button>
+      </van-form>
+      <!--/登录表单-->
     </div>
 </template>
 
 <script>
-    export default {
-        name: "LoginIndex"
+  export default {
+    name: "LoginIndex",
+    data(){
+      return {
+        user:{
+          mobile:'',
+          code:''
+        }
+      }
+    },
+    methods:{
+      onLogin() {
+
+      }
     }
+  }
 </script>
 
 <style scoped>
