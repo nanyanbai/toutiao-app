7df2f3f08dc34f734bd420ded5f7b3343f815501
nanweiwei <1115001559@qq.com>
2020-10-09 18:07:34 +0800

    用户频道列表

--- src/api/user.js
+++ src/api/user.js
@@ -41,3 +41,14 @@ export const getCurrentUser = () => {
     // }
   })
 }
+
+
+/**
+ * 获取用户频道列表
+ */
+export const getUserChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/channels'
+  })
+}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,12 +9,45 @@
           size="small"
         >搜索</van-button>
       </van-nav-bar>
+
+      <!-- 文章频道列表 -->
+      <van-tabs v-model="active">
+        <van-tab
+          v-for="item in  channels"
+          :key="item.id"
+          :title="item.name"
+        >{{ item.name }}的内容</van-tab>
+      </van-tabs>
+      <!-- /文章频道列表 -->
+
+
     </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+
 export default {
-  name: "HomeIndex"
+  name: "HomeIndex",
+  comments: {},
+  props: {},
+  data () {
+    return {
+      active: 0,
+      channels: [] // 频道列表
+    }
+  },
+  computed: {},
+  created() {
+    this.loadChannels()
+  },
+  methods: {
+    async loadChannels () {
+      const { data } = await getUserChannels()
+      // console.log(data);
+      this.channels = data.data.channels
+    }
+  }
 }
 </script>
 
